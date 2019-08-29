07b133d2cbd8c66d4e4511e66984695252806e6f
杨文波 <yangwenbo@middol.com>
2019-08-29 11:32:43 +0800

    aaaaaa

--- day1.py
+++ day1.py
@@ -1,26 +1,33 @@
 from __future__ import print_function
 import torch
 
-x = torch.empty(5, 3)
+# x = torch.empty(5, 3)
+# print(x)
+
+# x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
+# print(x)
+
+# x = torch.randn_like(x, dtype=torch.float)    # override dtype!
+# print(x)                                      # result has the same size
+
+# y = torch.rand(5, 3)
+# print(x + y)
+# print(torch.add(x, y))
+# result = torch.empty(5, 3)
+# # Addition: providing an output tensor as argument
+# torch.add(x, y, out=result)
+# print(result)
+
+# # adds x to y
+# y.add_(x)
+# print(y)
+
+# print(x[:, 1])
+# print(x[1, :])
+
+x = torch.randn(4,4)
+y = x.view(16)
 print(x)
-
-x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
-print(x)
-
-x = torch.randn_like(x, dtype=torch.float)    # override dtype!
-print(x)                                      # result has the same size
-
-y = torch.rand(5, 3)
-print(x + y)
-print(torch.add(x, y))
-result = torch.empty(5, 3)
-# Addition: providing an output tensor as argument
-torch.add(x, y, out=result)
-print(result)
-
-# adds x to y
-y.add_(x)
 print(y)
-
-print(x[:, 1])
-print(x[1, :])
+z = x.view(-1,8)
+print(z)
