0b6746a4ddb94e63aed44e6043467c31f017c304
杨文波 <yangwenbo@middol.com>
2019-08-29 11:20:52 +0800

    aaa

--- day1.py
+++ day1.py
@@ -9,3 +9,15 @@ print(x)
 
 x = torch.randn_like(x, dtype=torch.float)    # override dtype!
 print(x)                                      # result has the same size
+
+y = torch.rand(5, 3)
+print(x + y)
+print(torch.add(x, y))
+result = torch.empty(5, 3)
+# Addition: providing an output tensor as argument
+torch.add(x, y, out=result)
+print(result)
+
+# adds x to y
+y.add_(x)
+print(y)
