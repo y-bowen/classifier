1d427c3c52fca35abe95bed81598ca1300176853
杨文波 <yangwenbo@middol.com>
2019-08-29 11:16:52 +0800

    init

--- day1.py
+++ day1.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 import torch
 
-x = torch.empty(5, 3)
+x = x.new_ones(5, 3, dtype=torch.double)      # new_* methods take in sizes
 print(x)
+
+x = torch.randn_like(x, dtype=torch.float)    # override dtype!
+print(x)                                      # result has the same size
