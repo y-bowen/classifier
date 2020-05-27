3d367291fad0bf28d90fd7e0c2afe8dd52f7a6ef
杨文波 <yangwenbo@middol.com>
2020-05-27 18:00:55 +0800

    init

--- main.py
+++ main.py
@@ -190,14 +190,15 @@ def help():
 
 if __name__ == '__main__':
     # import fire
-
+    #
     # fire.Fire()
     loader = transforms.Compose([
         transforms.Resize(224),
         transforms.ToTensor()])
     data = Image.open("./data/train/POS-4441756.bmp")
     data = loader(data)
-    data = data.view(1, 1, 224, 224)
+    d = torch.Tensor(1,1,224,224)
+    d[1] = data
     c = torch.nn.Conv2d(1, 64, kernel_size=11, stride=4, padding=2)
-    data = c(data)
+    data = c(d)
     print(data.size())
