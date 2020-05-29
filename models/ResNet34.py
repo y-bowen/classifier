9a432c9c080272220cf8a6320b3559c29b5c678d
杨文波 <yangwenbo@middol.com>
2020-05-29 13:12:09 +0800

    init

--- models/ResNet34.py
+++ models/ResNet34.py
@@ -81,7 +81,6 @@ class ResNet34(BasicModule):
 
         x = F.avg_pool2d(x, 7)
         x = x.view(x.size(0), -1)
-        print(x.size())
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
