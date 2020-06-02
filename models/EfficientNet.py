f5a13eb7ecd1afb16ce4f5ef15002342b057975d
杨文波 <yangwenbo@middol.com>
2020-06-02 17:53:35 +0800

    init

--- models/EfficientNet.py
+++ models/EfficientNet.py
@@ -1,5 +1,7 @@
 from efficientnet_pytorch import EfficientNet
 
+from models.BasicModule import BasicModule
+
 efficientnet_b1 = EfficientNet.from_name("efficientnet-b1")
 efficientnet_b2 = EfficientNet.from_name("efficientnet-b2")
 efficientnet_b3 = EfficientNet.from_name("efficientnet-b3")
@@ -9,7 +11,10 @@ efficientnet_b6 = EfficientNet.from_name("efficientnet-b6")
 efficientnet_b7 = EfficientNet.from_name("efficientnet-b7")
 
 
-def efficientnet_b0():
-    efficientnet_b0 = EfficientNet.from_name("efficientnet-b0")
-    efficientnet_b0._fc.out_features = 2
-    return efficientnet_b0
+class Efficientnet_b0(BasicModule):
+    def __init__(self):
+        self.efficientnet_b0 = EfficientNet.from_name("efficientnet-b0")
+        self.efficientnet_b0._fc.out_features = 2
+
+    def forward(self, x):
+        return self.efficientnet_b0(x)
