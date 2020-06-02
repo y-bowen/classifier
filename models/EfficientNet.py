16a2eee12f5de4f7a14296baa79b2ac8b9a79da1
杨文波 <yangwenbo@middol.com>
2020-06-02 17:41:58 +0800

    init

---
+++ models/EfficientNet.py
@@ -0,0 +1,15 @@
+from efficientnet_pytorch import EfficientNet
+
+efficientnet_b1 = EfficientNet.from_name("efficientnet-b1")
+efficientnet_b2 = EfficientNet.from_name("efficientnet-b2")
+efficientnet_b3 = EfficientNet.from_name("efficientnet-b3")
+efficientnet_b4 = EfficientNet.from_name("efficientnet-b4")
+efficientnet_b5 = EfficientNet.from_name("efficientnet-b5")
+efficientnet_b6 = EfficientNet.from_name("efficientnet-b6")
+efficientnet_b7 = EfficientNet.from_name("efficientnet-b7")
+
+
+def efficientnet_b0(num_classes=2):
+    efficientnet_b0 = EfficientNet.from_name("efficientnet-b0")
+    efficientnet_b0._fc.out_features = num_classes
+    return efficientnet_b0
--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,4 @@
 from .AlexNet import AlexNet
 from .ResNet34 import ResNet34
 from .ResNet50 import ResNet50, ResNet101, ResNet152
-from efficientnet_pytorch import EfficientNet
-efficientnet_b0 = EfficientNet.from_name("efficientnet-b0")
-efficientnet_b1 = EfficientNet.from_name("efficientnet-b1")
-efficientnet_b2 = EfficientNet.from_name("efficientnet-b2")
-efficientnet_b3 = EfficientNet.from_name("efficientnet-b3")
-efficientnet_b4 = EfficientNet.from_name("efficientnet-b4")
-efficientnet_b5 = EfficientNet.from_name("efficientnet-b5")
-efficientnet_b6 = EfficientNet.from_name("efficientnet-b6")
-efficientnet_b7 = EfficientNet.from_name("efficientnet-b7")
+from .EfficientNet import efficientnet_b0
