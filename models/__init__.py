536310ce6e7ef22b04339d3bc7306be8a445d491
杨文波 <yangwenbo@middol.com>
2020-06-03 16:53:37 +0800

    init

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .AlexNet import AlexNet
 from .ResNet34 import ResNet34
 from .ResNet50 import ResNet50, ResNet101, ResNet152
 from .EfficientNet import Efficientnet_b0
+from .SENet import se_resnet50 as SEResNet50
