5e7982b9b74fc59f1d95deb13cc47904170e6eb0
杨文波 <yangwenbo@middol.com>
2020-06-03 17:25:22 +0800

    init

--- models/SENet.py
+++ models/SENet.py
@@ -70,9 +70,10 @@ class SEResNet(nn.Module):
     def __init__(self, block, layers, num_classes=2, input_channel=1):
         self.inplanes = 64
         super(SEResNet, self).__init__()
+        self.model_name = 'SEResNet'
         self.conv1 = Conv1(in_planes=input_channel, places=64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # self.relu = nn.ReLU(inplace=True)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
