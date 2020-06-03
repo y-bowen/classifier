b24b81f4b6436f04c10d035e4824cc2e646adb71
杨文波 <yangwenbo@middol.com>
2020-06-03 11:04:54 +0800

    init

--- main.py
+++ main.py
@@ -221,33 +221,21 @@ def transforms():
 def compute_mean_std():
     startt = 5000
     CNum = 10000  # 挑选多少图片进行计算
-    R_channel = 0
-    G_channel = 0
-    B_channel = 0
-    R_channel_square = 0
-    G_channel_square = 0
-    B_channel_square = 0
+    channel = 0
+    channel_square = 0
     pixels_num = 0
     filename = os.listdir(opt.train_data_root)
-    imgs = []
     for i in range(startt, startt + CNum):
         img = Image.open(os.path.join(opt.train_data_root, filename[i]))
-        h, w, _ = img.shape
+        h, w = img.size
         pixels_num += h * w  # 统计单个通道的像素数量
+        img = np.array(img)
+        temp = img[:, :, 0]
+        channel += np.sum(temp)
+        channel_square += np.sum(np.power(temp, 2.0))
 
-        R_temp = img[:, :, 0]
-        R_channel += np.sum(R_temp)
-        R_channel_square += np.sum(np.power(R_temp, 2.0))
-        G_temp = img[:, :, 1]
-        G_channel += np.sum(G_temp)
-        G_channel_square += np.sum(np.power(G_temp, 2.0))
-        B_temp = img[:, :, 2]
-        B_channel = B_channel + np.sum(B_temp)
-        B_channel_square += np.sum(np.power(B_temp, 2.0))
 
-    R_mean = R_channel / pixels_num
-    G_mean = G_channel / pixels_num
-    B_mean = B_channel / pixels_num
+    mean = channel / pixels_num
 
     """   
     S^2
@@ -258,12 +246,10 @@ def compute_mean_std():
     = sum(x^2)/N - x'^2
     """
 
-    R_std = np.sqrt(R_channel_square / pixels_num - R_mean * R_mean)
-    G_std = np.sqrt(G_channel_square / pixels_num - G_mean * G_mean)
-    B_std = np.sqrt(B_channel_square / pixels_num - B_mean * B_mean)
+    std = np.sqrt(channel_square / pixels_num - mean * mean)
 
-    print("R_mean is %f, G_mean is %f, B_mean is %f" % (R_mean, G_mean, B_mean))
-    print("R_std is %f, G_std is %f, B_std is %f" % (R_std, G_std, B_std))
+    print("mean is %f" % (mean))
+    print("std is %f" % (std))
 
 if __name__ == '__main__':
     import fire
